e9b490178c0353110594d8514123e4f886f974ab
Mastan Shaik <mastan30@gmail.com>
2020-05-01 18:42:39 -0400

    modified pom and controller to test run docker

--- src/main/java/com/cyri/businesslogic/controller/cyriController.java
+++ src/main/java/com/cyri/businesslogic/controller/cyriController.java
@@ -2,10 +2,11 @@ package com.cyri.businesslogic.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class cyriController {
 	 private static final Logger logger = LogManager.getLogger(cyriController.class);
@@ -13,6 +14,6 @@ public class cyriController {
 	    @GetMapping("/welcome")
 	    public String welcome() {
             logger.debug("Inside welcome method of controller");
-	        return "welcome"; 
+	        return "Business Logic welcome"; 
 	    }
 }
